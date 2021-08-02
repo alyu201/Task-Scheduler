a097d8e4fdb2e053e03afc5da99caed9e9462cf1
Kelvin Shen <wshe874@aucklanduni.ac.nz>
2021-08-02 18:16:35 +1200

    FIX - Add documentation for the input process class

--- TaskScheduling/src/GraphProcessing.java
+++ TaskScheduling/src/GraphProcessing.java
@@ -6,26 +6,42 @@ import org.graphstream.stream.file.FileSourceFactory;
 import java.io.IOException;
 import java.util.Iterator;
 
+/**
+This class is used for processing input dot file and output dot file.
+ @author kelvi
+ */
 public class GraphProcessing {
-    public static void main(String ... args) throws IOException {
-        String filePath = "C:\\Users\\kelvi\\Uni-Git\\project-1-project-1-team-6\\input.dot";
-        Graph g = new DefaultGraph("g");
-        FileSource fs = FileSourceFactory.sourceFor(filePath);
+    private Graph graph = new DefaultGraph("graph");
 
-        fs.addSink(g);
+
+    /**
+     * This method takes a path to a dot file and uses the GraphStream library to convert the file content to a graph.
+     * @throws IOException
+     * @author Kelvin
+     */
+    public void inputProcessing(String filePath) throws IOException {
+        FileSource fileSource = FileSourceFactory.sourceFor(filePath);
+        fileSource.addSink(this.graph);
         try {
-            fs.readAll(filePath);
-            System.out.println(g);
-            System.out.println(g.getEdgeCount());
-            Iterator i = g.iterator();
+            fileSource.readAll(filePath);
+            System.out.println(graph);
+            System.out.println(graph.getEdgeCount());
+            Iterator i = graph.iterator();
             while (i.hasNext()) {
                 System.out.println(i.next());
             }
 
-        } catch( IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            fs.removeSink(g);
+            fileSource.removeSink(graph);
         }
     }
+
+    /**
+     * This method will write the graph that is currently in the system to a dot file.
+     */
+    public void outputProcessing(String filePath) throws IOException {
+        graph.write(filePath);
     }
+}
--- TaskScheduling/src/Main.java
+++ TaskScheduling/src/Main.java
@@ -1,2 +1,11 @@
+import java.io.IOException;
+
 public class Main {
+    public static void main(String... args) throws IOException {
+        GraphProcessing graph = new GraphProcessing();
+        String filePath = "C:\\Users\\kelvi\\Uni-Git\\project-1-project-1-team-6\\input.dot";
+        graph.inputProcessing(filePath);
+        filePath = "C:\\Users\\kelvi\\Uni-Git\\project-1-project-1-team-6\\input1.dot";
+        graph.outputProcessing(filePath);
+    }
 }

fe49c50afb2ed27a05b9b772f48e84c68e667a7a
Kelvin Shen <wshe874@aucklanduni.ac.nz>
2021-08-03 18:16:52 +1200

    NEW - Exception model

---
+++ src/main/java/Model/InvalidInputFilenameException.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class InvalidInputFilenameException extends Exception{
+    InvalidInputFilenameException(String errorMessage){
+        super(errorMessage);
+    }
+
+}
--- src/main/java/Model/Main.java
+++ src/main/java/Model/Main.java
@@ -11,39 +11,71 @@ public class Main {
         // TODO: 8/3/2021  Check number of argument must be at least 2
         // TODO: 8/3/2021 Creat Exceptions
 
-        //Process the input dot file
-        String firstArgument = args[0];
-        System.out.println(firstArgument);
-        String filePath = Paths.get(firstArgument).toAbsolutePath().toString();
-        GraphProcessing graph = new GraphProcessing();
-        graph.inputProcessing(filePath);
-        graph.outputProcessing(filePath);
+        try {
+            //Process the input dot file
+            String filePath = Paths.get(args[0]).toAbsolutePath().toString();
+            if (!filePath.contains(".dot")){
+                throw new InvalidInputFilenameException("The input filename needs a .dot extension.");
+            }
+            GraphProcessing graph = new GraphProcessing();
+            graph.inputProcessing(filePath);
 
-        //Process the number of processor argument
-        String secondArgument = args[1];
-        int numberOfProcess = Integer.parseInt(secondArgument);
-        System.out.println(numberOfProcess);
+            //Process the number of processor argument
+            int numberOfProcess = Integer.parseInt(args[1]);
+            System.out.println(numberOfProcess);
 
-        //Process the other arguments
-        // TODO: 8/3/2021 processing other arguments
+            //Process the other arguments
+            processingOptions(args, graph);
+
+            //Example output a dot file
+            graph.outputProcessing(filePath);
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+
+
+
+
+    }
+
+    public static void processingOptions(String[] args, GraphProcessing graph) throws IOException {
         int numberArg = args.length;
         ArrayList<String> arguments = new ArrayList<>();
         while (numberArg>2){
             numberArg--;
             arguments.add(args[numberArg]);
         }
-        System.out.println(arguments);
 
-    }
-    public void coreArgProcedure(int numCores){
-        // TODO: 8/3/2021  implementation for number of cores
+        if (arguments.contains("-p")){
+            int indexOfp = arguments.indexOf("-p");
+            int numCore = Integer.parseInt(arguments.get(indexOfp-1));
+            coreArgProcedure(numCore);
+        }
+
+        if (arguments.contains("-v")){
+            visualArgProcedure();
+        }
+
+        if (arguments.contains("-o")){
+            int indexOfo = arguments.indexOf("-o");
+            String outputFilename = arguments.get(indexOfo-1);
+            outputArgProcedure(outputFilename,graph);
+        }
     }
 
-    public void visualArgProcedure(){
-        // TODO: 8/3/2021 implement for visualization
+    public static void coreArgProcedure(int numCores){
+        System.out.println("the number of core is "+numCores);
+        // TODO: 8/3/2021 implementation for parallelization required
     }
 
-    public void outputArgProcedure(String outputFilename){
-        // TODO: 8/3/2021 implement for output argument
+    public static void visualArgProcedure(){
+        System.out.println("require visualization");
+        // TODO: 8/3/2021 implementation for visualization required
+    }
+
+    public static void outputArgProcedure(String outputFilename, GraphProcessing graph) throws IOException {
+        graph.outputProcessing(outputFilename);
     }
 }

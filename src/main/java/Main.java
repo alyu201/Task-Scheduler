11b90ac1398e65cc67cc77843b394a1d6aec098e
Kelvin Shen <wshe874@aucklanduni.ac.nz>
2021-08-03 13:46:30 +1200

    FIX - Change Structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {

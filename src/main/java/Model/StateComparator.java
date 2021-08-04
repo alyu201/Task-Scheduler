05f856e3b4e500b4e6d6faeb42b32f8494edbb49
Sherman <shermanchin33@gmail.com>
2021-08-04 16:06:57 +1200

    NEW - Add State cost function underestimate

--- src/main/java/Model/StateComparator.java
+++ src/main/java/Model/StateComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class StateComparator implements Comparator<State> {
     @Override
-    public int compare(Object o1, Object o2) {
-        return 0;
+    public int compare(State state1, State state2) {
+        return state1.getUnderestimate() - state2.getUnderestimate();
     }
 }

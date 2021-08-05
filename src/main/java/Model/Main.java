8d44271958b9c8b7fff56b4cb0efab3535d162be
Kelvin Shen <wshe874@aucklanduni.ac.nz>
2021-08-05 13:22:43 +1200

    NEW - New jar file

--- src/main/java/Model/Main.java
+++ src/main/java/Model/Main.java
@@ -28,6 +28,8 @@ public class Main {
 
             System.out.println("the program ends");
 
+            System.exit(0);
+
         }catch(Exception e){
             System.out.println(e);
         }

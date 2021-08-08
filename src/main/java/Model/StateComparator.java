df2cb638ae0007c063c6a490d5567bffeda4054a
Sherman <shermanchin33@gmail.com>
2021-08-08 21:28:45 +1200

    NEW - Add documentation for StateComparator

--- src/main/java/Model/StateComparator.java
+++ src/main/java/Model/StateComparator.java
@@ -2,6 +2,10 @@ package Model;
 
 import java.util.Comparator;
 
+/**
+ * This comparator allows different State to be compared based on their underestimates (cost function)
+ * author: Sherman Chin
+ */
 public class StateComparator implements Comparator<State> {
     @Override
     public int compare(State state1, State state2) {

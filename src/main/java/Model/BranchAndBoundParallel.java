7c374d9224f290898653feed27a48d4f3c72cbf3
Sherman <shermanchin33@gmail.com>
2021-08-29 19:06:39 +1200

    FIX - Revert to previous parallelization technique with modification
    The parallelization now explores depth wise instead of breadth wise by
    changing fork and join sequence

--- src/main/java/Model/BranchAndBoundParallel.java
+++ src/main/java/Model/BranchAndBoundParallel.java
@@ -6,20 +6,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.RecursiveAction;
 
 /**
  * This class is responsible for recursively compute the final goal state.
  * This class is called from the BranchAndBoundScheduler class.
- * @Author Kelvin
+ *
+ * @Author Kelvin Shen and Sherman Chin
  */
 public class BranchAndBoundParallel extends RecursiveAction {
     private BranchAndBoundScheduler _branchAndBoundScheduler;
     private State _currentState;
 
-    public BranchAndBoundParallel(BranchAndBoundScheduler branchAndBoundScheduler, State state){
+    public BranchAndBoundParallel(BranchAndBoundScheduler branchAndBoundScheduler, State currenState) {
         _branchAndBoundScheduler = branchAndBoundScheduler;
-        _currentState = state;
+        _currentState = currenState;
 
     }
 
@@ -28,6 +30,50 @@ public class BranchAndBoundParallel extends RecursiveAction {
      */
     @Override
     protected void compute() {
-        _branchAndBoundScheduler.exploreState(_currentState);
+        // only count number of processors/threads if parallelisation is required
+        if (Main.PARALLELISATIONFLAG) {
+            Visualiser.incrThreadCount();
+        }
+
+        if (_branchAndBoundScheduler.getClosedList().contains(_currentState) || _currentState.getUnderestimate() >= _branchAndBoundScheduler.getUpperBound()) {
+            return;
+        }
+
+        if (_branchAndBoundScheduler.goalStateReached(_currentState)) {
+            _branchAndBoundScheduler.checkAndUpdateUpperBound(_currentState);
+        } else {
+            List<Node> schedulableTasks = _branchAndBoundScheduler.getNextTasks(_currentState);
+            _branchAndBoundScheduler.fixTaskOrder(schedulableTasks, _currentState);
+            PriorityQueue<State> childStates = _branchAndBoundScheduler.addChildStates(_currentState, schedulableTasks);
+
+            List<BranchAndBoundParallel> listActions = allocateThreadTask(childStates);
+
+            for (State i: childStates) {
+                BranchAndBoundParallel childTask = new BranchAndBoundParallel(_branchAndBoundScheduler, i);
+                childTask.compute();
+            }
+
+            for (BranchAndBoundParallel childThread : listActions) {
+                childThread.join();
+            }
+        }
+        _branchAndBoundScheduler.updateClosedList(_currentState);
+    }
+
+    private synchronized List<BranchAndBoundParallel> allocateThreadTask (PriorityQueue<State> states) {
+        int availableThreads = BranchAndBoundScheduler.Pool.getParallelism() - BranchAndBoundScheduler.Pool.getPoolSize();
+
+        List<BranchAndBoundParallel> listActions = new ArrayList<>();
+        for (int i = 0; i < availableThreads; i++) {
+            //Remain one task for original thread
+            if (states.size() <= 1) {
+                break;
+            }
+            BranchAndBoundParallel task = new BranchAndBoundParallel(_branchAndBoundScheduler, states.poll());
+            task.fork();
+            listActions.add(task);
+        }
+
+        return listActions;
     }
 }
